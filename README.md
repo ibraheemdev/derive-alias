ef5df905f359d32adc90ee66ad96e526e760a08c
ibraheemdev <ibrah1440@gmail.com>
2021-04-08 17:34:01 -0400

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# Derive Alias
+
+[![Documentation](https://img.shields.io/badge/docs-0.1.0-4d76ae?style=for-the-badge)](https://docs.rs/derive-alias/0.1.0)
+[![Version](https://img.shields.io/crates/v/derive-alias?style=for-the-badge)](https://crates.io/crates/derive-alias)
+[![License](https://img.shields.io/crates/l/derive-alias?style=for-the-badge)](https://crates.io/crates/derive-alias)
+[![Actions](https://img.shields.io/github/workflow/status/ibraheemdev/derive-alias/Rust/master?style=for-the-badge)](https://github.com/ibraheemdev/derive-alias/actions)
+
+Provides a way to alias mutliple derives as one:
+```rust
+use derive_alias::derive_alias;
+
+// Generates a macro (`derive_cmp`) that will attach the listed derives to a given item
+derive_alias! {
+    derive_cmp => #[derive(Eq, PartialEq, Ord, PartialOrd)]
+}
+
+// Attach the derives to `Foo`
+derive_cmp! { struct Foo; }
+```
+
+You can create multiple aliases at a time:
+```rust
+# use derive_alias::derive_alias;
+
+derive_alias! {
+    derive_cmp => #[derive(Eq, PartialEq, Ord, PartialOrd)],
+    derive_other => #[derive(Copy, Clone)]
+}
+
+derive_cmp! { struct Foo; }
+derive_other! { struct Bar; }
+```
---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+//! Provides a way to alias mutliple derives as one:
+//! ```rust
+//! use derive_alias::derive_alias;
+//!
+//! // Generates a macro (`derive_cmp`) that will attach the listed derives to a given item
+//! derive_alias! {
+//!     derive_cmp => #[derive(Eq, PartialEq, Ord, PartialOrd)]
+//! }
+//!
+//! // Attach the derives to `Foo`
+//! derive_cmp! { struct Foo; }
+//! ```
+//!
+//! You can create multiple aliases at a time:
+//! ```rust
+//! # use derive_alias::derive_alias;
+//!
+//! derive_alias! {
+//!     derive_cmp => #[derive(Eq, PartialEq, Ord, PartialOrd)],
+//!     derive_other => #[derive(Copy, Clone)]
+//! }
+//!
+//! derive_cmp! { struct Foo; }
+//! derive_other! { struct Bar; }
+//! ```
+
+/// Refer to the [crate level documentation](crate) for details.
+#[macro_export]
+macro_rules! derive_alias {
+    ($($name:ident => #[derive($($derive:ident),*)] $(,)?)*) => {
+        $(
+            macro_rules! $name {
+                ($i:item) => {
+                    #[derive($($derive),*)]
+                    $i
+                }
+            }
+        )*
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::Debug;
+
+    derive_alias! {
+        derive_one => #[derive(Copy, Clone, Debug)],
+        derive_two => #[derive(Eq, PartialEq, Ord, PartialOrd)]
+    }
+
+    derive_one! {
+        struct Foo(i32);
+    }
+
+    derive_two! {
+        struct Bar(i32);
+    }
+
+    #[test]
+    fn a_test() {
+        assert_impl_one(Foo(100));
+        assert_impl_two(Bar(200));
+    }
+
+    fn assert_impl_one<T>(_: T)
+    where
+        T: Copy + Clone + Debug,
+    {
+    }
+
+    fn assert_impl_two<T>(_: T)
+    where
+        T: Eq + PartialEq + Ord + PartialOrd,
+    {
+    }
+
+    #[allow(unused_macros)]
+    mod test_trailing_comma {
+        derive_alias! {
+            foo => #[derive()]
+            bar => #[derive()]
+            baz => #[derive()]
+        }
+
+        derive_alias! {
+            foo => #[derive()],
+            bar => #[derive()],
+            baz => #[derive()],
+        }
+    }
+}
